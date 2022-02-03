41a343bb5c8c41acd4f5202a44f7922597cd6951
PHessberger <philipph128@gmail.com>
2022-02-04 00:26:33 +0100

    I didn't event touch the file, don't know how it got changed...

---
+++ app/src/main/java/com/example/steammarketapp/APIHandler.java
@@ -0,0 +1,8 @@
+package com.example.steammarketapp;
+
+public class APIHandler {
+
+    String testURL = "https://steamcommunity.com/market/priceoverview/?appid=730&currency=3&market_hash_name=StatTrak™%20M4A1-S%20|%20Hyper%20Beast%20(Factory%20New)";
+
+    String testURL2 = "https://steamcommunity.com/market/priceoverview/?appid=730&currency=3&market_hash_name=StatTrak™%20M4A1-S%20|%20Hyper%20Beast%20(Factory%20New)";
+}
